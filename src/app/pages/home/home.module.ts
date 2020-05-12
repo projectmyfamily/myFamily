cc8e724b98e10207886718c05c72a6344da8d3ec
MathLemos <matheusbastosddd@gmail.com>
2020-05-12 12:48:14 -0300

    matheus-12052020

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { ModalperfilPage } from '../modalperfil/modalperfil.page';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { HomePage } from './home.page';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, ModalperfilPage],
+  entryComponents: [ModalperfilPage]
 })
 export class HomePageModule {}
--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
+
+  sair(){
+
+    this.navCtrl.navigateForward('/login');
+  }
 }
