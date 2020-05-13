0c594b42a9e3cee0335156d632a6f97d1101a230
MathLemos <matheusbastosddd@gmail.com>
2020-05-12 21:07:19 -0300

    matheus-12052020-1.1

--- src/app/pages/modalpin/modalpin.page.ts
+++ src/app/pages/modalpin/modalpin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modalpin',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalpinPage implements OnInit {
 
-  constructor() { }
+  constructor(public modalCtrl: ModalController, public navCtrl: NavController) { }
 
   ngOnInit() {
   }
+
+  voltar() {
+
+    this.modalCtrl.dismiss();
+  }
+
+  entrar() {
+
+    this.navCtrl.navigateForward('/tabs/home');
+  }
+
+  esqueceusenha() {
+
+    
+  }
+
 
 }
